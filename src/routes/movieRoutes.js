fff5298e42d45f6226c69bdce0fe0242ce818918
meanev56 <emekanjoku86@gmail.com>
2026-04-08 07:37:15 +0100

    integrating prysma db

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -2,8 +2,21 @@ import express from "express";
 
 const router = express.Router();
 
-router.length("/hello", (req, res) => {
-    res.json({message: "hello"})
+router.get("/", (req, res) => {
+    res.json({message: "hello"});
 })
+
+router.post("/", (req, res) => {
+    res.json({message: "hello"});
+})
+
+router.put("/", (req, res) => {
+    res.json({message: "hello"});
+})
+
+router.delete("/", (req, res) => {
+    res.json({message: "hello"});
+})
+
 
 export default router;

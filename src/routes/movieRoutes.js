d726fe7ba133ff95ca4e552312bab483a1fa1931
meanev56 <emekanjoku86@gmail.com>
2026-04-09 14:48:17 +0100

    jwt token

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -3,20 +3,19 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json({message: "hello"});
-})
+  res.json({ httpMethod: "get" });
+});
 
 router.post("/", (req, res) => {
-    res.json({message: "hello"});
-})
+  res.json({ httpMethod: "post" });
+});
 
 router.put("/", (req, res) => {
-    res.json({message: "hello"});
-})
+  res.json({ httpMethod: "put" });
+});
 
 router.delete("/", (req, res) => {
-    res.json({message: "hello"});
-})
-
+  res.json({ httpMethod: "delete" });
+});
 
 export default router;
